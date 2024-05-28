06d233d463f9c1d4cdcd6436a82f14e9e3e719e3
RavinduLaksahan <ravindulaksahan@gmail.com>
2024-05-28 21:36:01 +0530

    second commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,23 @@
-
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import Slider from 'react-slick';
 import s1 from '../assets/SlideShow1.jpg'
 import s2 from '../assets/SlideShow2.jpg'
 import s3 from '../assets/Slideshow3.jpg'
 import s4 from '../assets/Slideshow4.jpg'
+import { FaUserTie } from "react-icons/fa";
+import { MdOutlineFax } from "react-icons/md";
+import { GiRotaryPhone } from "react-icons/gi";
+import { PiOfficeChairFill } from "react-icons/pi";
+import { FaShop } from "react-icons/fa6";
+import { IoMailSharp } from "react-icons/io5";
+import { FaAddressCard } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 
 const HomePage = () => {
+
+  const location = useLocation();
 
   const settings = {
     dots: true,
@@ -17,62 +26,95 @@ const HomePage = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 10000,
+    autoplaySpeed: 5000,
+    ssEase: 'ease-in-out',
   };
 
   const slides = [
-    {
-      image: s1,
-      title: 'Elevate Your Bathing Experience',
-      titleColor: 'text-gray-900',
-      description: 'Step into luxury with our modern bathroom collection. Impeccable design meets functionality, creating a serene oasis in your home.',
-      descriptionColor: 'text-gray-900',
-      titlePosition: 'center',
-      descriptionPosition: 'center',
-    },
-    {
-      image: s2,
-      title: 'Wall Tiles That Define Elegance',
-      titleColor: 'text-gray-200',
-      description: 'Transform your living space with our exquisite wall tiles. Crafted with precision and style, they add sophistication to every corner of your home.',
-      descriptionColor: 'text-gray-200',
-      titlePosition: 'center',
-      descriptionPosition: 'center'
-    },
-    {
-      image: s3,
-      title: 'Floor Tiles for a Lasting Impression',
-      titleColor: 'text-gray-800',
-      description: 'Make a statement with our premium floor tiles. From classic to contemporary, our collection offers durability and beauty to complement any room.',
-      descriptionColor: 'text-gray-700',
-      titlePosition: 'center',
-      descriptionPosition: 'center'
-    },
-    {
-      image: s4,
-      title: 'Create Memories in Style',
-      titleColor: 'text-white',
-      description: 'Welcome guests into your stunning living space. With our versatile ceramic products, every gathering becomes an occasion to cherish.',
-      descriptionColor: 'text-gray-200',
-      titlePosition: 'center',
-      descriptionPosition: 'center'
-    },
-    
+      {
+        image: s1,
+        title: 'Elevate Your Bathing Experience!',
+        titleColor: 'text-gray-200',
+        description: 'Experience luxury with our modern bathroom collection.',
+        descriptionColor: 'text-gray-200',
+        titlePosition: 'center',
+        descriptionPosition: 'center',
+      },
+      {
+        image: s2,
+        title: 'Wall Tiles That Define Elegance!',
+        titleColor: 'text-gray-200',
+        description: 'Transform your space with our exquisite wall tiles.',
+        descriptionColor: 'text-gray-200',
+        titlePosition: 'center',
+        descriptionPosition: 'center'
+      },
+      {
+        image: s3,
+        title: 'Floor Tiles for a Lasting Impression!',
+        titleColor: 'text-gray-200',
+        description: 'Make a statement with our premium floor tiles.',
+        descriptionColor: 'text-gray-200',
+        titlePosition: 'center',
+        descriptionPosition: 'center'
+      },
+      {
+        image: s4,
+        title: 'Create Memories in Style!',
+        titleColor: 'text-gray-200',
+        description: 'Welcome guests into your stunning living space.',
+        descriptionColor: 'text-gray-200',
+        titlePosition: 'center',
+        descriptionPosition: 'center'
+      },
+      
   ];
+  
   
 
   return (
     <div>
-      <nav className="bg-orange-700 bg-opacity-10 shadow fixed w-full top-0 z-50">
-        <div className="container mx-auto flex justify-between items-center py-8" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
+      <style>
+        {`
+          html {
+            scroll-behavior: smooth;
+          }
+
+          .text-gradient {
+            background-image: radial-gradient(circle, transparent 0%, rgba(255,255,255,0.5) 50%, rgba(255,255,255,0.8) 80%, rgba(255,255,255,0.9) 100%),linear-gradient(90deg, #000000, #241e2f, #40484a);
+            background-size: 200% auto;
+            background-clip: text;
+            -webkit-background-clip: text;
+            color: transparent;
+            animation: gradient 10s linear infinite;
+          }
+
+          @keyframes gradient {
+            0% {
+              background-position: 100% 50%; 
+            }
+            50% {
+              background-position: 0% 50%; 
+            }
+            100% {
+              background-position: 100% 50%; 
+            }
+          }
+        `}
+      </style>
+
+      <nav className="bg-black bg-opacity-30 shadow fixed w-full top-0 z-50 ">
+        <div className="container mx-auto flex justify-between items-center py-8 " style={{ fontFamily: 'Rajdhani, sans-serif' }}>
           <div className="flex items-center">
-            <Link to="/" className="text-xl font-bold mr-10 ml-10">Ramitha Ceramics</Link>
+            <Link to="/" className="text-3xl font-bold mr-10 ml-10">
+              <span className=" text-gray-200">Ramitha Ceramics</span>
+            </Link>
           </div>
-          <ul className="flex font-bold space-x-20 mr-20">
-            <li><a href="#home" className="hover:text-gray-500">HOME</a></li>
-            <li><Link to="/products" className="hover:text-gray-500">PRODUCTS</Link></li>
-            <li><a href="#about" className="hover:text-gray-500">ABOUT US</a></li>
-            <li><a href="#contact" className="hover:text-gray-500">CONTACT US</a></li>
+          <ul className="flex font-bold space-x-20 mr-20 text-gray-500">
+            <li><a href="#home" className={`hover:text-gray-200 ${location.hash === '#home' ? 'text-white' : ''}`}>HOME</a></li>
+            <li><Link to="/products" className={`hover:text-gray-200 ${location.pathname === '/products' ? 'text-white' : ''}`}>PRODUCTS</Link></li>
+            <li><a href="#about" className={`hover:text-gray-200 ${location.hash === '#about' ? 'text-white' : ''}`}>ABOUT US</a></li>
+            <li><a href="#contact" className={`hover:text-gray-200 ${location.hash === '#contact' ? 'text-white' : ''}`}>CONTACT US</a></li>
           </ul>
         </div>
       </nav>
@@ -84,10 +126,11 @@ const HomePage = () => {
           {slides.map((slide, index) => (
             <div key={index} className="w-full h-full relative">
               <img src={slide.image} alt={`Slide ${index + 1}`} className="w-full h-full object-cover" />
+              <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
               <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
                 <div className="text-white text-center" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
-                  <h1 className={`text-5xl font-bold ${slide.titleColor} ${slide.titlePosition === 'left' ? 'absolute left-4' : ''}`}>{slide.title}</h1>
-                  <p className={`mt-5 font-bold ${slide.descriptionColor} ${slide.descriptionPosition === 'left' ? 'absolute left-4' : ''}`}>{slide.description}</p>
+                  <h1 className={`text-gradient text-6xl font-bold ${slide.titleColor} ${slide.titlePosition === 'left' ? 'absolute left-4' : ''}`}>{slide.title}</h1>
+                  <p className={`mt-8 font-bold ${slide.descriptionColor} ${slide.descriptionPosition === 'left' ? 'absolute left-4' : ''}`}>{slide.description}</p>
                 </div>
               </div>
             </div>
@@ -95,22 +138,70 @@ const HomePage = () => {
         </Slider>
       </section>
 
-      <section id="about" className="h-screen flex items-center justify-center bg-white mt-10">
-        <div className="max-w-4xl mx-auto text-center p-8" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
-          <h2 className="text-4xl font-bold mb-6">About Us</h2>
-          <p className="text-lg mb-4">Welcome to Ramitha Ceramics, where artistry meets functionality. We pride ourselves on offering an extensive selection of high-quality ceramic products, ranging from elegant wall tiles to durable floor tiles and exquisite bathroom fittings.</p>
-          <p className="text-lg mb-4">Our mission is to enhance the beauty and comfort of your home with our meticulously crafted ceramics. Each piece is designed to reflect both contemporary and classic styles, ensuring that you find the perfect match for your aesthetic preferences.</p>
-          <p className="text-lg">At Ramitha Ceramics, we are committed to sustainability and innovation. We use eco-friendly materials and processes to create products that not only look good but also contribute to a greener planet. Join us in transforming your spaces with our beautiful and sustainable ceramic solutions.</p>
+
+      <section id="about" className="h-screen flex items-center justify-center bg-white mt-6" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
+        <div className="max-w-4xl mx-auto text-center p-8">
+        <h2 className="text-5xl font-bold mb-6 relative overflow-hidden">
+          <span className="text-transparent bg-gradient-to-r bg-clip-text from-gray-900 to-gray-700 transition-all duration-500 ease-in-out hover:from-white  hover:to-gray-900 text-gradient">About Ramitha Ceramic</span>
+        </h2>
+          <p className="text-lg mb-4 text-gray-900 font-bold">Welcome to Ramitha Ceramics, where artistry meets functionality!</p>
+          <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6 justify-center bg-opacity-10">
+            <div className="flex-1 w-96 h-64 p-6 bg-gradient-to-b from-red-300 to-gray-200 shadow-lg flex items-center justify-center rounded-lg hover:scale-105 transition duration-300">
+              <p className="text-lg">Discover our exquisite range of ceramic tiles and fittings, crafted with passion and precision.</p>
+            </div>
+            <div className="flex-1 w-96 h-64 p-6 bg-gradient-to-b from-red-300 to-gray-200 shadow-lg flex items-center justify-center rounded-lg hover:scale-105 transition duration-300">
+              <p className="text-lg">We blend traditional craftsmanship with modern design, offering sustainable and stylish solutions for every home.</p>
+            </div>
+            <div className="flex-1 w-96 h-64 p-6 bg-gradient-to-b from-red-300 to-gray-200 shadow-lg flex items-center justify-center rounded-lg hover:scale-105 transition duration-300">
+              <p className="text-lg">Transform your spaces with our unique and beautiful ceramic collections. Experience the Ramitha Ceramics difference today!</p>
+            </div>
+          </div>
+          {/* <div className="w-20 h-2 bg-black mt-8 mx-auto"></div> */}
         </div>
       </section>
 
-        <section id="contact" className="h-screen flex items-center justify-center bg-gray-100">
-          <div className="max-w-2xl text-center" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
-            <h2 className="text-3xl font-bold mb-4">Contact Us</h2>
-            
+      
+
+      <section id="contact" className="h-screen flex items-center justify-center bg-white mt-4" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
+        <div className="max-w-6xl mx-auto text-center p-8" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
+          <h2 className="text-5xl font-bold mb-6 relative overflow-hidden">
+            <span className="text-transparent bg-gradient-to-r bg-clip-text from-gray-900 to-gray-700 transition-all duration-500 ease-in-out hover:from-white hover:to-gray-900 text-gradient">Contact Us</span>
+          </h2>
+          <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6 justify-center bg-opacity-10">
+            <div className="flex-1 w-96 h-64 p-6 bg-gradient-to-b from-gray-600 to-gray-200 shadow-lg flex flex-col justify-center rounded-lg items-center hover:scale-105 transition duration-300">
+              <h3 className="text-xl font-bold mb-4"><FaUserTie className="inline mr-2" /> Director</h3>
+              <p>J.M. Ramitha Jayasundara</p>
+              <p><GiRotaryPhone className="inline mr-2" /><a href="tel:+94763942353">076-394 2353</a></p>
+              <p><IoMailSharp className="inline mr-2" /><a href="mailto:ramithaj18@gmail.com">ramithaj18@gmail.com</a></p>
+            </div>
+            <div className="flex-1 w-96 h-64 p-6 bg-gradient-to-b from-gray-600 to-gray-200 shadow-lg flex flex-col justify-center rounded-lg items-center hover:scale-105 transition duration-300">
+              <h3 className="text-xl font-bold mb-4"><PiOfficeChairFill className="inline mr-2" />Office</h3>
+              <p><FaAddressCard className="inline mr-2" />No. 14, Kandy Road, Gurudeniya.</p>
+              <p><GiRotaryPhone className="inline mr-2" /><a href="tel:+94812406835">Tel: 081-24 06 835</a></p>
+              <p><MdOutlineFax className="inline mr-2" /><a href="tel:+94812406835">Fax: 081-24 06 835</a></p>
+            </div>
+            <div className="flex-1 w-96 h-64 p-6 bg-gradient-to-b from-gray-600 to-gray-200 shadow-lg flex flex-col justify-center rounded-lg items-center hover:scale-105 transition duration-300">
+              <h3 className="text-xl font-bold mb-4"><FaShop className="inline mr-2" />Showrooms</h3>
+              <div className="flex justify-center space-x-10">
+                <div>
+                  <p><FaLocationDot className="inline mr-2" />Gurudeniya</p>
+                  <p><GiRotaryPhone className="inline mr-2" /><a href="tel:+94812052230">081-20 52 230</a></p>
+                  <p><GiRotaryPhone className="inline mr-2" /><a href="tel:+94812052224">081-20 52 224</a></p>
+                </div>
+                <div>
+                  <p><FaLocationDot className="inline mr-2" />Kandy</p>
+                  <p><GiRotaryPhone className="inline mr-2" /><a href="tel:+94812222494">081-22 22 494</a></p>
+                </div>
+              </div>
+            </div>
           </div>
-        </section>
+        </div>
+      </section>
       </main>
+      
+      <footer className="bg-gray-700 text-white text-center py-4">
+        <p>© {new Date().getFullYear()} Ramitha Enterprises(Pvt) Ltd. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
